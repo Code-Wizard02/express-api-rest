8897d45c7296b035bec48000a4ec460e58407b2a
Kevin Hernández <21160665@itoaxaca.edu.mx>
2025-03-24 13:16:30 -0600

    Adding User Model

---
+++ models/User.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+
+// Hash de la contraseña antes de guardar
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+export default mongoose.model('User', userSchema);
